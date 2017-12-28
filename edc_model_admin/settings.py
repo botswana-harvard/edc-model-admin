9dd8b47c76595f1a6a35d4170862aa7073af202a
erikvw <ew2789@gmail.com>
2017-12-28 21:11:16 +0200

    update settings

--- edc_model_admin/settings.py
+++ edc_model_admin/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_model_admin'
+ETC_DIR = os.path.join(BASE_DIR, 'etc')
+SITE_ID = 40
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'edc_model_admin.apps.AppConfig',
 ]
 
