ac1fa05ab744cc73253f956893562d78ac0b2239
Itumeleng Mosweu <imosweuk@gmail.com>
2022-03-18 18:08:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-model-admin',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
